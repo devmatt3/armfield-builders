e2ed2db0b31e8c7f246cfaf213927857881ab31c
Matthew Armfield <devmatt3@gmail.com>
2020-12-09 13:49:05 -0500

    Add in styling for maintenance page Refactor styling project structure Add in favicons

--- src/pages/Maintenance.js
+++ src/pages/Maintenance.js
@@ -4,11 +4,14 @@ import styles from '../styles/Maintenance.module.scss';
 const Home = () => {
     return (
         <div className={styles.MaintenanceWrapper}>
-            <div className={styles.MaintenanceCopy}>
-                Just like our homes, the site is currently <span>under construction</span>.
+            <div className={styles.MaintenanceBackground}></div>
+            <div className={styles.MaintenanceCopyWrap}>
+                <h4>
+                    Just like our homes, the site is currently <span>under construction</span>.
+                </h4>
+                <h4>Check back later to see the progress!</h4>
+                <h5>- Armfield Builders</h5>
             </div>
-            <p className={styles.MaintenanceDisclaimer}>Check back later to see the progress!</p>
-            <p>-Armfield<span>Builders</span></p>
         </div>
     )
 }
